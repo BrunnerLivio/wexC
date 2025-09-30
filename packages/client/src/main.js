bfbbe7cf9426fb44ec1460d911961e5a2d853a43
Livio Brunner <livio.brunner@students.fhnw.ch>
2025-09-30 15:49:42 +0200

    remove websocket for now

--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -1,29 +1,29 @@
 // @ts-check
 import { monkeyBall } from './monkeyball/monkeyball.js';
-const socket = new WebSocket("ws://localhost:8081");
+// const socket = new WebSocket("ws://localhost:8081");
 
 
-socket.onopen = () => {
-  console.log("Connected to server");
-  sendMessage("rotate_left", { angle: 15 });
-};
+// socket.onopen = () => {
+//   console.log("Connected to server");
+//   sendMessage("rotate_left", { angle: 15 });
+// };
 
-socket.onmessage = (event) => {
-  /**
-   * @type {import('../../server/src/message.js').Message}
-   */
-  const data = JSON.parse(event.data);
-  console.log(data);
-};
+// socket.onmessage = (event) => {
+//   /**
+//    * @type {import('../../server/src/message.js').Message}
+//    */
+//   const data = JSON.parse(event.data);
+//   console.log(data);
+// };
 
-/**
- *
- * @param {import('../../server/src/message.js').Action} action
- * @param {import('../../server/src/message.js').Message['payload']} payload
- */
-const sendMessage = (action, payload = {}) => {
-  socket.send(JSON.stringify({ action, payload }));
-};
+// /**
+//  *
+//  * @param {import('../../server/src/message.js').Action} action
+//  * @param {import('../../server/src/message.js').Message['payload']} payload
+//  */
+// const sendMessage = (action, payload = {}) => {
+//   socket.send(JSON.stringify({ action, payload }));
+// };
 
 
 monkeyBall(document.getElementById('area'));
